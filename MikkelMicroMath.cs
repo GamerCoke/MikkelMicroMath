5494f7d47dde00b351a75ca0a2c72e567643a947
Mikkel Barfod <mib010@edu.zealand.dk>
2025-04-10 10:41:22 +0200

    add Add function comment

--- MikkelMicroMath.cs
+++ MikkelMicroMath.cs
@@ -8,6 +8,12 @@ namespace MikkelMicroMath
 {
     public class MikkelMicroMath
     {
+        /// <summary>
+        ///  Lægger to tal sammen
+        /// </summary>
+        /// <param name="a">første tal</param>
+        /// <param name="b">andet tal</param>
+        /// <returns>retunere et hel tal der er sum af a og b</returns>
         public int Add(int a, int b)
         {
             return a + b;
