afc827c5d0846b6c481fde44998d0ee6c8a2c10d
SaraIversen <sara.skovgaard.iversen@gmail.com>
2025-04-10 11:33:16 +0200

    uhji

---
+++ Hungry.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MikkelMicroMath
+{
+    public static class Hungry
+    {
+        public static Cake cake = new Cake();
+
+        public static void Eat()
+        {
+            for (int i = 0; i < cake.slices; i++)
+            {
+                Console.WriteLine("eating cake slice");
+            }
+        }
+    }
+
+    public class Cake
+    {
+        public int slices = 4;
+    }
+}
